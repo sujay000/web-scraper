e6bdfb57b78e81d39ec25b60032efe53250d978d
sujay000 <sujaysapkal22@gmail.com>
2021-11-22 16:39:12 +0530

    scrape update
    more scraped items added in the frontend part

--- app.js
+++ app.js
@@ -1,12 +1,23 @@
 const feedDisplay = document.querySelector('#feed');
 
+const titleDisplay = document.querySelector('#title');
+const descDisplay = document.querySelector('#desc');
+const imageDisplay = document.querySelector('#image');
+const faviconDisplay = document.querySelector('#favicon');
+
 
 
 fetch('http://localhost:8000/')
    .then(response => response.json())
    .then(data => {
+       titleDisplay.insertAdjacentHTML("beforeend",`<h1>`+ data.title +`</h1>`);
+       descDisplay.insertAdjacentHTML("beforeend",`<h2>`+ data.description +`</h2>`);
+       imageDisplay.insertAdjacentHTML("beforeend", `<img src= "`+ data.image + `" alt="img">` ); 
+       faviconDisplay.insertAdjacentHTML("beforeend", `<img src= "`+ data.favicon + `" alt="favicon">` ); 
+
+      console.log(data.favicon);
        data.articles.forEach(article => {
-           const articleItem = `<div><h3>` + article.title + `</h3><p>` + article.url + `</p></div>`
+           const articleItem = `<div><h3>` + article.title + `</h3><p><a href ="`+article.url +`">` + article.url + `</a></p></div>`
            feedDisplay.insertAdjacentHTML("beforeend", articleItem)
        })
       
