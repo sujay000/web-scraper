3be8a55cf8dd73e10600291693a499d2d972daa6
sujay000 <sujaysapkal22@gmail.com>
2021-11-23 14:21:59 +0530

    implement scraping
    used form to send a post request to the server and then send scrapd data

--- app.js
+++ app.js
@@ -1,24 +1,74 @@
-const feedDisplay = document.querySelector('#feed');
+const express = require("express");
+const app = express();
+const bodyParser = require('body-parser');
+const axios = require('axios');
+const cheerio = require('cheerio');
 
-const titleDisplay = document.querySelector('#title');
-const descDisplay = document.querySelector('#desc');
-const imageDisplay = document.querySelector('#image');
-const faviconDisplay = document.querySelector('#favicon');
+app.use(express.static("public"))
+
+app.use(bodyParser.urlencoded({extended:true}));
+
+app.get("/",(req,res)=>{
+    res.sendFile(__dirname+"/index.html");
+})
+
+app.post("/scrape",(req,res)=>{
+    
+    const webUrl = req.body.webUrl;
+    
+
+    axios(webUrl)
+    .then(response => {
+        const html = response.data;
+        const $ = cheerio.load(html);
+        const articles = [];
+        const title = $('title').first().text();
+        const favicon = $('link[rel="shorcut icon"]').attr('href');
+    
+        const getMetaTag =(name) => 
+           $(`meta[name=${name}]`).attr(`content`)||
+           $(`meta[property="og:${name}"]`).attr('content') ||
+           $(`meta[property = "twitter:${name}]`).attr('content'); 
+    
+        const description = getMetaTag('description');
+        const image = getMetaTag('image');
+    
+       const all = {
+          title,
+          description,
+          image,
+          favicon,
+          articles
+       };
+    
+        $('.fc-item__title', html).each(function () { 
+            const title = $(this).text();
+            const url = $(this).find('a').attr('href');
+            articles.push({
+                title,
+                url
+            })
+        })
+        res.write("<h1>"+title +"</h1>")
+        // res.write("desc ");
+        // res.write("image ");
+        // res.write("favicon ");
+        // articles.forEach(article => {
+        //     const articleItem = "<div><h3>" + article.title + "</h3><p>" + article.url + "</p></div>";
+        //     res.write(articleItem);
+        // })
+
+        res.write("<html><body>hi hello</body></html>");
+        console.log(articles[0].title);
+        
+        
+        res.send();
+    
+    
+    }).catch(err => console.log(err));
+
+})
 
 
 
-fetch('http://localhost:8000/')
-   .then(response => response.json())
-   .then(data => {
-       titleDisplay.insertAdjacentHTML("beforeend",`<h1>`+ data.title +`</h1>`);
-       descDisplay.insertAdjacentHTML("beforeend",`<h2>`+ data.description +`</h2>`);
-       imageDisplay.insertAdjacentHTML("beforeend", `<img src= "`+ data.image + `" alt="img">` ); 
-       faviconDisplay.insertAdjacentHTML("beforeend", `<img src= "`+ data.favicon + `" alt="favicon">` ); 
-
-      console.log(data.favicon);
-       data.articles.forEach(article => {
-           const articleItem = `<div><h3>` + article.title + `</h3><p><a href ="`+article.url +`">` + article.url + `</a></p></div>`
-           feedDisplay.insertAdjacentHTML("beforeend", articleItem)
-       })
-      
-  }).catch(err => console.log(err))
+app.listen(3000,()=>{console.log("port is running")})
